765755cc531c34ab06f037e876c5416b446cf5d0
Syrus Dark <naveen@syrusdark.website>
2020-12-17 01:08:31 +0530

    Add a Check

---
+++ updator/check_update.py
@@ -0,0 +1,22 @@
+import requests
+import json
+import os
+from pathlib import Path
+
+access_token = os.getenv("GITHUB_TOKEN")
+FINAL_FILE = Path(__file__).parent.parent / "versions.json"
+API_URL = "https://api.github.com/repos/naveen521kk/pango-build/releases"
+headers = {
+    "Accept": "application/vnd.github.v3+json",
+    "Authorization": f"token {access_token}",
+}
+with open(FINAL_FILE, "r") as f:
+    data = json.load(f)
+pango_version = data["pango"]
+
+content = requests.get(API_URL, headers).json()
+
+if content[0]["tag_name"] != f"v{pango_version}":
+    print("true")
+else:
+    print("false")
