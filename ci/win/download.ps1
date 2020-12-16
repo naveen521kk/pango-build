5fb7459825d9f8f5995598c7f4a0352c5c32d84c
Syrus Dark <naveen@syrusdark.website>
2020-12-16 23:50:15 +0530

    Updator

--- ci/win/download.ps1
+++ ci/win/download.ps1
@@ -1,71 +1,73 @@
-$COMMIT_RELEASE = $env:COMMIT_RELEASE_CAIRO
-$PANGO_VERSION = $env:PANGO_VERSION
-$FRIBIDI_VERSION = $env:FRIBIDI_VERSION
-$HARFBUZZ_VERSION = $env:HARFBUZZ_VERSION
-$GLIB_VERSION = $env:GLIB_VERSION
+#note: Should be invoked from root of repo
+$versions = Get-Content .\versions.json | ConvertFrom-Json 
+$COMMIT_RELEASE = $versions.cairo
+$PANGO_VERSION = $versions.pango
+$FRIBIDI_VERSION = $versions.fribidi
+$HARFBUZZ_VERSION = $versions.harfbuzz
+$GLIB_VERSION = $versions.glib
 
-echo "Getting Cairo"
+Write-Output "Getting Cairo"
 curl https://gitlab.freedesktop.org/cairo/cairo/-/archive/$($COMMIT_RELEASE)/cairo-$($COMMIT_RELEASE).tar.gz -o cairo.tar.gz
 tar -xf cairo.tar.gz
 Move-Item -Path cairo-$COMMIT_RELEASE -Destination cairo -Force
       
-echo "Getting pkg-config"
+Write-Output "Getting pkg-config"
 curl https://github.com/pkgconf/pkgconf/archive/pkgconf-1.7.0.zip -o pkgconf.zip
 7z x pkgconf.zip
 Move-Item -Path pkgconf-* -Destination pkgconf -Force
 
-echo "Getting xz Utils"
+Write-Output "Getting xz Utils"
 curl https://tukaani.org/xz/xz-5.2.5-windows.zip -OutFile xz.zip
 7z x xz.zip -oC:\xzUtil
 $env:PATH = "C:\xzUtil\bin_x86-64;$env:PATH"
 
-echo "Getting Pango"
+Write-Output "Getting Pango"
 curl https://ftp.gnome.org/pub/GNOME/sources/pango/$(("$PANGO_VERSION" -split '\.')[0,1] -join ".")/pango-$($PANGO_VERSION).tar.xz -OutFile pango.tar.xz
 xz -d pango.tar.xz
 tar -xf pango.tar
 Move-Item -Path pango-* -Destination pango -Force
 
-echo "Getting Fribidi"
-curl https://github.com/fribidi/fribidi/releases/download/v$($FRIBIDI_VERSION)/fribidi-$($FRIBIDI_VERSION).tar.xz -OutFile fribidi.tar.xz
+Write-Output "Getting Fribidi"
+curl https://github.com/fribidi/fribidi/releases/download/$($FRIBIDI_VERSION)/fribidi-$($FRIBIDI_VERSION).tar.xz -OutFile fribidi.tar.xz
 xz -d fribidi.tar.xz
 tar -xf fribidi.tar
 Move-Item -Path fribidi-* -Destination fribidi -Force
 
-echo "Getting Harfbuzz"
+Write-Output "Getting Harfbuzz"
 curl https://github.com/harfbuzz/harfbuzz/releases/download/$($HARFBUZZ_VERSION)/harfbuzz-$($HARFBUZZ_VERSION).tar.xz -OutFile harfbuzz.tar.xz
 xz -d harfbuzz.tar.xz
 tar -xf harfbuzz.tar
 Move-Item -Path harfbuzz-* -Destination harfbuzz -Force
 
-echo "Getting Glib"
+Write-Output "Getting Glib"
 curl https://ftp.gnome.org/pub/gnome/sources/glib/$(("$GLIB_VERSION" -split '\.')[0,1] -join ".")/glib-$GLIB_VERSION.tar.xz -OutFile glib.tar.xz
 xz -d glib.tar.xz
 tar -xf glib.tar
 Move-Item -Path glib-* -Destination glib -Force
 
-echo "Setting Up Meson and Ninja"
+Write-Output "Setting Up Meson and Ninja"
 $env:PATH = "C:\Python38-x64;C:\Python38-x64\Scripts;$env:PATH"
 python -m pip install --upgrade pip
 pip install --upgrade meson==0.55.3 ninja
       
-echo "Copying x86 files"
+Write-Output "Copying x86 files"
 mkdir x86
-cd x86
+Set-Location x86
 Copy-Item -Path "$PWD\..\cairo" -Destination "$PWD\cairo" -Recurse
 Copy-Item -Path "$PWD\..\pango" -Destination "$PWD\pango" -Recurse
 Copy-Item -Path "$PWD\..\fribidi" -Destination "$PWD\fribidi" -Recurse
 Copy-Item -Path "$PWD\..\harfbuzz" -Destination "$PWD\harfbuzz" -Recurse
 Copy-Item -Path "$PWD\..\pkgconf" -Destination "$PWD\pkgconf" -Recurse
 Copy-Item -Path "$PWD\..\glib" -Destination "$PWD\glib" -Recurse
-cd ../
+Set-Location ../
 
-echo "Copying x64 files"
+Write-Output "Copying x64 files"
 mkdir x64
-cd x64
+Set-Location x64
 Copy-Item -Path "$PWD\..\cairo" -Destination "$PWD\cairo" -Recurse
 Copy-Item -Path "$PWD\..\pango" -Destination "$PWD\pango" -Recurse
 Copy-Item -Path "$PWD\..\fribidi" -Destination "$PWD\fribidi" -Recurse
 Copy-Item -Path "$PWD\..\harfbuzz" -Destination "$PWD\harfbuzz" -Recurse
 Copy-Item -Path "$PWD\..\pkgconf" -Destination "$PWD\pkgconf" -Recurse
 Copy-Item -Path "$PWD\..\glib" -Destination "$PWD\glib" -Recurse
-cd ../
+Set-Location ../
---
+++ updator/main.py
@@ -0,0 +1,77 @@
+import json
+import logging
+import re
+from pathlib import Path
+
+import requests
+import yaml
+
+logging.basicConfig(level=logging.DEBUG)
+DEPS_FILE = Path(__file__).parent / "dependency.yml"
+FINAL_FILE = Path(__file__).parent.parent / "versions.json"
+with open(DEPS_FILE) as f:
+    logging.debug("Opening File at %s", DEPS_FILE.absolute())
+    deps_info = yaml.load(f, Loader=yaml.FullLoader)
+version_info = {}
+
+
+class GithubHandler:
+    def __init__(self, api_url: str = None, replace_in_files: dict = None) -> None:
+        self.api_url = api_url
+        self.replace_in_files = replace_in_files
+        self.get_version()
+        self.update_file()
+
+    def get_version(self) -> None:
+        version_re = re.compile(self.replace_in_files["tag_filter"]["matching"])
+        logging.debug("Getting %s", self.api_url)
+        content = requests.get(self.api_url).json()
+        for version in content:
+            matching = version_re.match(version["name"])
+            if matching:
+                logging.info("Got Latest Version as %s", version["name"])
+                self.version = version["name"]
+                break
+
+    def update_file(self):
+        logging.info(
+            "Setting %s version  to %s", self.replace_in_files["name"], self.version
+        )
+        version_info[self.replace_in_files["name"]] = self.version
+
+
+class GitlabHandler:
+    def __init__(self, api_url: str = None, replace_in_files: dict = None) -> None:
+        self.api_url = api_url
+        self.replace_in_files = replace_in_files
+        self.get_version()
+        self.update_file()
+
+    def get_version(self) -> None:
+        version_re = re.compile(self.replace_in_files["tag_filter"]["matching"])
+        logging.debug("Getting %s", self.api_url)
+        content = requests.get(self.api_url).json()
+        for version in content:
+            matching = version_re.match(version["name"])
+            if matching:
+                logging.info("Got Latest Version as %s", version["name"])
+                self.version = version["name"]
+                break
+
+    def update_file(self):
+        logging.info(
+            "Setting %s version  to %s", self.replace_in_files["name"], self.version
+        )
+        version_info[self.replace_in_files["name"]] = self.version
+
+
+for name in deps_info:
+    lib = deps_info[name]
+    if lib["type"] == "gitlab":
+        GitlabHandler(lib["api_url"], replace_in_files=lib["replace_in_files"])
+    else:
+        GithubHandler(lib["api_url"], replace_in_files=lib["replace_in_files"])
+
+with open(FINAL_FILE, "w") as f:
+
+    data = json.dump(version_info, f, indent=4)
